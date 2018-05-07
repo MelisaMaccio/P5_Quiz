3b396980c4731d03ff7e8802eaa6bf8c88fe0fcb
MelisaMaccio <melisa.maccio@hotmail.com>
2018-05-07 16:05:53 +0200

    P5_v32

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-//const Sequelize = require("sequelize");
-//const {models} = require("../models");
+const Sequelize = require("sequelize");
+const {models} = require("../models");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
